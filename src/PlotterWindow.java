1f98beb1d1a476d5649472ccc45f65955fae293b
Guntram Trebs <77011066+guntramtrebs@users.noreply.github.com>
2024-05-22 14:46:35 +0200

    JPanel SinusPlotter plotti hinzugefügt

--- src/PlotterWindow.java
+++ src/PlotterWindow.java
@@ -1,8 +1,15 @@
 import javax.swing.*;
 
 public class PlotterWindow extends JFrame {
+
+    private SinusPlotter plotti;
+
     public PlotterWindow() {
         super("Plotter für Sinus und Kosinus");
+
+        plotti = new SinusPlotter();
+
+        this.add(plotti);
 
         this.setSize(420, 420);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
