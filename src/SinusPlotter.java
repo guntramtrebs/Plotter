af5d51151d11a2e592a66fbbf15690f00e0393e7
Guntram Trebs <77011066+guntramtrebs@users.noreply.github.com>
2024-05-22 15:04:21 +0200

    plotti malt Koordinateneinteilung von x-Achse und y-Achse

--- src/SinusPlotter.java
+++ src/SinusPlotter.java
@@ -8,5 +8,10 @@ public class SinusPlotter extends JPanel {
         g.setColor(Color.BLACK);
         g.drawLine(200, 0, 200, 400);
         g.drawLine(0, 200, 400, 200);
+
+        for (int i=20; i<400; i += 20) {
+            g.drawLine(i, 198, i, 202);
+            g.drawLine(198, i, 202, i);
+        }
     }
 }
