301f5376e2919bb096b683c7e8225e7ef83fafdf
Guntram Trebs <77011066+guntramtrebs@users.noreply.github.com>
2024-05-23 01:38:09 +0200

    added cyan cotangent, removed tangens typo

--- src/SinusPlotter.java
+++ src/SinusPlotter.java
@@ -61,7 +61,7 @@ public class SinusPlotter extends JPanel {
                 g.drawString(String.valueOf(i), originX + 6, originY - i * pixelPerUnit + 3);
             }
         }
-        g.drawString("y", originX -12, 10);
+        g.drawString("y", originX - 12, 10);
 
         // black x axis
         g.setColor(Color.BLACK);
@@ -71,39 +71,54 @@ public class SinusPlotter extends JPanel {
         for (int i = leftUnit; i < rightUnit; i++) {
             g.drawLine(originX + i * pixelPerUnit, originY - 2, originX + i * pixelPerUnit, originY + 2);
             if (i != 0) {
-                g.drawString(String.valueOf(i), originX + i * pixelPerUnit -6 , originY + 14);
+                g.drawString(String.valueOf(i), originX + i * pixelPerUnit - 6, originY + 14);
             }
         }
-        g.drawString("x", panelWidth -12, originY + 10);
+        g.drawString("x", panelWidth - 12, originY + 10);
 
         g.setColor(Color.RED);
         g.drawString("y=sin(x)", 10, 45);
 
         g.setColor(Color.BLUE);
-        g.drawString("y=cos(x)", 10, 65);
+        g.drawString("y=cos(x)", 10, 60);
 
-        // TODO: check if values of tangens and cotangens are valid
+        g.setColor(Color.GREEN);
+        g.drawString("y=tan(x)", 10, 75);
+
+        g.setColor(Color.CYAN);
+        g.drawString("y=cot(x)", 10, 90);
+
+        g.setColor(Color.RED);
+        g.drawString("TODO: work on tangent and cotangent", 10, 110);
+
+
+        // TODO: check if values of tangent and cotangent are valid (infinity, undefined)
+        // TODO: do not connect if undefined between points
         int oldX = 0;
         double oldSinusY = originY - Math.sin((oldX - originX) / (double) pixelPerUnit) * pixelPerUnit;
         double oldCosinusY = originY - Math.cos((oldX - originX) / (double) pixelPerUnit) * pixelPerUnit;
-        double oldTangensY = originY - Math.tan((oldX - originX) / (double) pixelPerUnit) * pixelPerUnit;
+        double oldTangentY = originY - Math.tan((oldX - originX) / (double) pixelPerUnit) * pixelPerUnit;
+        double oldCotangentY = originY - 1 / Math.tan((oldX - originX) / (double) pixelPerUnit) * pixelPerUnit;
+
         for (int x = 1; x < panelWidth; x++) {
             double sinusY = originY - Math.sin((x - originX) / (double) pixelPerUnit) * pixelPerUnit;
             double cosinusY = originY - Math.cos((x - originX) / (double) pixelPerUnit) * pixelPerUnit;
-            double tangensY = originY - Math.tan((x - originX) / (double) pixelPerUnit) * pixelPerUnit;
+            double tangentY = originY - Math.tan((x - originX) / (double) pixelPerUnit) * pixelPerUnit;
+            double cotangentY = originY - 1 / Math.tan((x - originX) / (double) pixelPerUnit) * pixelPerUnit;
 
             g.setColor(Color.RED);
             g.drawLine(oldX, (int) oldSinusY, x, (int) sinusY);
             g.setColor(Color.BLUE);
             g.drawLine(oldX, (int) oldCosinusY, x, (int) cosinusY);
             g.setColor(Color.GREEN);
-            g.drawLine(oldX, (int) oldTangensY, x, (int) tangensY);
-
-
+            g.drawLine(oldX, (int) oldTangentY, x, (int) tangentY);
+            g.setColor(Color.CYAN);
+            g.drawLine(oldX, (int) oldCotangentY, x, (int) cotangentY);
 
             oldSinusY = sinusY;
             oldCosinusY = cosinusY;
-            oldTangensY = tangensY;
+            oldTangentY = tangentY;
+            oldCotangentY = cotangentY;
             oldX = x;
         }
     }
