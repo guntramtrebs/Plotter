da94c35ca83ac188aa0e0cf60bf43e147f20a3bd
Guntram Trebs <77011066+guntramtrebs@users.noreply.github.com>
2024-05-22 15:22:53 +0200

    plotti malt gelben Hintergrund

--- src/SinusPlotter.java
+++ src/SinusPlotter.java
@@ -5,6 +5,9 @@ public class SinusPlotter extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(0,0,420, 400);
+
         g.setColor(Color.BLACK);
         g.drawLine(200, 0, 200, 400);
         g.drawLine(0, 200, 400, 200);
