ef33d18a72339e5736a39a62d07b85e3f8deb9fe
Guntram Trebs <77011066+guntramtrebs@users.noreply.github.com>
2024-05-22 14:43:09 +0200

    Plotter-Fenster wird geöffnet

---
+++ src/PlotterWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class PlotterWindow extends JFrame {
+    public PlotterWindow() {
+        super("Plotter für Sinus und Kosinus");
+
+        this.setSize(420, 420);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
--- src/Start.java
+++ src/Start.java
@@ -1,6 +1,6 @@
 public class Start {
     public static void main(String[] args) {
 
-        System.out.println("Projekt erstellt");
+        new PlotterWindow();
     }
 }
