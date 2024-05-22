91ade61d8afeda3b4e2f360fd88de8c52e8b8e5e
Guntram Trebs <77011066+guntramtrebs@users.noreply.github.com>
2024-05-22 14:49:29 +0200

    plotti malt y-Achse als Strecke

--- src/SinusPlotter.java
+++ src/SinusPlotter.java
@@ -1,4 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SinusPlotter extends JPanel {
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.drawLine(200, 0, 200, 400);
+    }
 }
